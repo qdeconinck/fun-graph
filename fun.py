1bed89ccea438daedd105dd7ae866b275d57ad50
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2021-07-12 15:44:49 +0200

    add support for all alphabetical characters

--- fun.py
+++ fun.py
@@ -3,14 +3,19 @@ import random
 from symbol import ScatterDataGenerator
 
 sdg = ScatterDataGenerator()
-dx, dy = sdg.get_data(['suicide', 'toi'])
+dx, dy = sdg.get_data(['suis-je un beau', 'graphe pour ta', 'these ?'])
 
 dx = [x + random.uniform(-0.02, 0.02) for x in dx]
 dy = [y + random.uniform(-0.02, 0.02) for y in dy]
 
-for _ in range(1000):
-    dx.append(random.uniform(min(dx), max(dx)))
-    dy.append(random.uniform(min(dy), max(dy)))
+min_dx = min(dx)
+max_dx = max(dx)
+min_dy = min(dy)
+max_dy = max(dy)
+
+for _ in range(5000):
+    dx.append(random.uniform(min_dx, max_dx))
+    dy.append(random.uniform(min_dy, max_dy))
 
 plt.scatter(dx, dy)
 
